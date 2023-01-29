377093cafa73849a3002c49ead7de7a808cf4c1e
Ray Ortiz <oray@bgsu.edu>
2023-01-29 14:24:27 -0500

    npm install dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,13 @@
+const { network } = require('hardhat');
+
 require('@nomicfoundation/hardhat-toolbox');
 
 module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
+  }
+  networks:{
+
   }
 };
