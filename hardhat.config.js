eb99948f84beb94b445fdd06d86220781322824e
Ray Ortiz <oray@bgsu.edu>
2023-01-29 18:14:33 -0500

    Migrating from Waffle

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-chai-matchers");
 
 module.exports = {
   solidity: "0.8.17",
